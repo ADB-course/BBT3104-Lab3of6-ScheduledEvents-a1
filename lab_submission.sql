3cfaa8e994348ee8f891ebf908dc2bcc649101f8
Jeanette <Jeanette.Omondi@strathmore.edu>
2024-09-30 22:06:14 +0300

    lab3

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,8 @@
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+CREATE EVENT EVN_average_customer_waiting_time_every_1_hour
+ON SCHEDULE EVERY 1 HOUR
+DO
+  INSERT INTO customer_service_kpi (average_waiting_time, record_time)
+  SELECT AVG(customer_service_total_wait_time_minutes), NOW()
+  FROM customer_service_ticket
+  WHERE customer_service_ticket_raise_time >= NOW() - INTERVAL 1 HOUR;
